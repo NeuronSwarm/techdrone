fd7f5d6582b1dacfecf419a8cae0b74f2c1e7ff8
Ty Connors <tconn89@gmail.com>
2017-10-13 14:25:19 -0400

    Add logo

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,7 @@ window.addEventListener('load',function(){
     var welcome = document.querySelector('.greet'),
         subtext = document.querySelector('.subTexts'),
         form    = document.querySelector('.sub'),
+        container=document.querySelector('.social'),
         follow  = document.querySelector('.followUs'),
         social  = document.querySelectorAll('.socialIcon'),
         delay = 1000; 
@@ -10,6 +11,7 @@ window.addEventListener('load',function(){
     setTimeout(function(){welcome.style.top='0';},delay);
     setTimeout(function(){subtext.style.bottom = '0%';},delay*2);
     setTimeout(function(){subtext.style.bottom = '-100%';},delay*4);
+    setTimeout(function(){container.style.display = 'block';},delay*5);
     setTimeout(function(){form.style.opacity='1';},delay*5);
     setTimeout(function(){follow.style.bottom='0%';},delay*6);
     setTimeout(
