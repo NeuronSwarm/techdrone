d22536c5ef6ec8fd418e7a0d8783237383afe9bd
Ty Connors <tconn89@gmail.com>
2017-11-27 20:38:09 -0500

    Fix mobile controls

--- public/js/bezier/touchControls.js
+++ public/js/bezier/touchControls.js
@@ -3,9 +3,7 @@
   exports.MobileControls = function(){}
   MobileControls.onTouchMove = function(e){
     var touch = e.originalEvent.touches[0]
-    var cp2 = new Point(touch.pageX, touch.pageY)
-    console.log('touch controls: ' + cp2.toString())
-    return bezierDraw(e, cp2)
+    return cp2 = new Point(touch.pageX, touch.pageY)
   }
 
 })(window)
